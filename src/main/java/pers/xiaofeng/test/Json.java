e274081024a8404bf8e77cf033aa0f5690834f4b
韩晓峰 <2220342736@qq.com>
2020-09-17 15:18:51 +0800

    九大内部排序算法中的直接插入、折半插入、冒泡、简单选择和快速排序算法的代码实现

--- src/main/java/pers/xiaofeng/test/Json.java
+++ src/main/java/pers/xiaofeng/test/Json.java
@@ -21,11 +21,12 @@ public class Json {
     private static Map<String, Integer> userOptMap = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        test();
+        test1();
+        test2();
 
     }
 
-    public static void test() throws IOException {
+    public static void test1() throws IOException {
         StringBuffer sb = null;
         String jsonStr = null;
         try {
@@ -43,32 +44,51 @@ public class Json {
             jsonStr = sb.toString();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
         }
-
         // 使用Gson将字符串序列化为UserOpt对象
         Gson gson = new Gson();
         List<UserOpt> list = gson.fromJson(jsonStr, new TypeToken<List<UserOpt>>() {
         }.getType());
         // 将序列化后的对象中的相应的字段拼接组成userOptMap的key,最后将对应的用户服操作方式赋值给userOptMap的value
         for (int i = 0; i < list.size(); ++i) {
-            String str = String.valueOf(list.get(i).getGameId()) + String.valueOf(list.get(i).getOpt())
-                    + String.valueOf(list.get(i).getParameter());
-            userOptMap.put(str,list.get(i).getModule());
+            String str = String.valueOf(list.get(i).getDeskId()) + String.valueOf(list.get(i).getGameId())
+                    + String.valueOf(list.get(i).getOpt()) + String.valueOf(list.get(i).getParameter());
+            userOptMap.put(str, list.get(i).getModule());
         }
 
-        // for (int i = 0; i < userOpts.length; ++i) {
-        //     for (int j = 0; j < 2; ++j) {
-        //         System.out.println(userOpts[i][j]);
-        //     }
+        // for(String key : userOptMap.keySet() ){
+        //     System.out.println(key + ":" + userOptMap.get(key));
         // }
+    }
 
-        for(String key : userOptMap.keySet() ){
-            System.out.println(key);
-        }
+    public static void test2() {
 
-        for(Integer value : userOptMap.values()){
-            System.out.println(value);
+        long deskId = 1197078913024000l;                    // 桌子id
+        int gameId = 71;                                    // 游戏id
+        int opt = 206;                                      // 操作码
+        int parameter = 1;                                  // 操作码参数
+
+        String key;     // 操作配置表中的主键（桌子id + 游戏id + 操作码 + 操作码参数）
+        int module;     // 对应的处理模式
+
+        // 拼接key
+        String desk1 = Long.toBinaryString(deskId);
+        String desk2 = desk1.substring(1, 3);
+        int desk = Integer.parseInt(desk2, 2);
+        key = String.valueOf(desk) + String.valueOf(gameId)
+                + String.valueOf(opt) + String.valueOf(parameter);
+        System.out.println("桌子id解析：" + deskId);
+        System.out.println("二进制 = " + desk1);
+        System.out.println("取二进制前两位 = " + desk2);
+        System.out.println("取二进制前两位转为int = " + desk);
+        try {
+            // 获取key对应的module
+            module = userOptMap.get(key);
+
+            System.out.println("key =" + key);
+            System.out.println("module =" + module);
+        } catch (Exception e) {
+            return;
         }
     }
 }
--- src/main/java/pers/xiaofeng/test/UserOpt.java
+++ src/main/java/pers/xiaofeng/test/UserOpt.java
@@ -6,10 +6,20 @@ package pers.xiaofeng.test;
  * @date：2020/9/3/10:14
  */
 public class UserOpt {
-    private int gameId;
-    private int opt;
-    private int parameter;
-    private int module;
+
+    private int deskId;             // 桌子id（前两位 0：普通房间 1：金币场 2：比赛场）
+    private int gameId;             // 游戏id
+    private int opt;                // 操作码
+    private int parameter;          // 操作码参数
+    private int module;             // 对用用户服处理方式
+
+    public int getDeskId() {
+        return deskId;
+    }
+
+    public void setDeskId(int deskId) {
+        this.deskId = deskId;
+    }
 
     public int getGameId() {
         return gameId;
